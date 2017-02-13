1aa2f70593e264b3bee619fd3b29cb4c84c5c2a0
Guilherme Rambo <insidegui@gmail.com>
2017-02-13 15:37:38 -0200

    Added Carthage support

---
+++ EDStarRating-Mac/EDStarRating-Mac.h
@@ -0,0 +1,19 @@
+//
+//  EDStarRating-Mac.h
+//  EDStarRating-Mac
+//
+//  Created by Guilherme Rambo on 13/02/17.
+//  Copyright © 2017 Peixe Urbano. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EDStarRating-Mac.
+FOUNDATION_EXPORT double EDStarRating_MacVersionNumber;
+
+//! Project version string for EDStarRating-Mac.
+FOUNDATION_EXPORT const unsigned char EDStarRating_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDStarRating_Mac/PublicHeader.h>
+
+
---
+++ Framework/EDStarRatingModule.h
@@ -0,0 +1,19 @@
+//
+//  EDStarRating.h
+//  EDStarRating
+//
+//  Created by Guilherme Rambo on 13/02/17.
+//  Copyright © 2017 Peixe Urbano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EDStarRating.
+FOUNDATION_EXPORT double EDStarRatingVersionNumber;
+
+//! Project version string for EDStarRating.
+FOUNDATION_EXPORT const unsigned char EDStarRatingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDStarRating/PublicHeader.h>
+
+#import <EDStarRating/EDStarRating.h>
